6c9f4d58e83da21f295cb497ba12ab7ed3408a26
Dinesh Kumar <159294017+DineshKumar930@users.noreply.github.com>
2026-03-18 10:23:18 +0530

    Update AuthController.java

--- src/main/java/com/nit/dk/controller/AuthController.java
+++ src/main/java/com/nit/dk/controller/AuthController.java
@@ -11,7 +11,7 @@ import com.nit.dk.service.AdminService;
 
 @RestController
 @RequestMapping("/api/admin")
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "*")
 public class AuthController {
 
     @Autowired
